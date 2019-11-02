cc122344191cbd817c0240ac7d77d0a8af2cc186
Kevin Michael <biobot.mike@gmail.com>
2019-11-03 00:23:54 +0200

    Initialize UI & showProfile

---
+++ app.js
@@ -0,0 +1,30 @@
+// Init GitHub
+const github = new GitHub;
+console.log(github);
+// Init UI
+const ui = new UI;
+console.log(ui);
+// Search input
+const username = document.getElementById('username');
+
+// Search input event listener
+username.addEventListener('keyup', (e) => {
+  // Get input text
+  const userInput = e.target.value;
+
+  if (userInput !== '') {
+    // console.log(userInput);
+    // Make http call
+    github.getUser(userInput)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+        } else {
+          // Show profile
+          ui.showProfile(data.profile);
+        }
+      });
+  } else {
+    // Clear profile
+  }
+});
